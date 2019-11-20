dcf214201234314ca5ec8047a22fc04f4d858b49
Kurt Bauer <kurtbauer55@protonmail.com>
2019-11-20 11:13:48 -0500

    import createStore/applyMiddleware from redux

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux'
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
