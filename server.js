4292ede54a553a5932d9e62f6feee8f02564d0b9
Kurt Bauer <kurtbauer55@protonmail.com>
2019-11-20 10:32:32 -0500

    install new packages

--- server.js
+++ server.js
@@ -2,3 +2,9 @@ var express = require('express');
 var stormpath = require('express-stormpath');
 
 var app = express();
+
+app.use(stormpath.init(app, {
+    web: {
+        produces: ['application/json']
+    }
+}));
