d4ff31f72bc4380cd7966d1c6dcdc669471bbc19
Kurt Bauer <kurtbauer55@protonmail.com>
2019-11-20 10:38:18 -0500

    create a new express application instance

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
 var webpack = require('webpack')
 var webpackDevMiddleware = require('webpack-dev-middleware')
 var webpackHotMiddleware = require('webpack-hot-middleware')
+
+var app = new (require('express'))()
